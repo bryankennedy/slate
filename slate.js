e81fb7a61737f0c7095184eefb399d77537330e4
bryan kennedy <bkennedy@smm.org>
2013-08-02 16:57:28 -0500

    Cleanup and disabling features I don't use
    Focusing on the few features I actually use.
    
    Putting the key combos near the actual window commands. They were a bit
    too absctracted before, making it difficult to see how the key command
    relates to the function.

--- slate.js
+++ slate.js
@@ -11,17 +11,14 @@ S.cfga({
 });
 
 // Monitors
-var monLaptop = "1680x1050";
+//var monLaptop = "1680x1050";
 var monDell = "1920x1080";
 
+//
 // Operations
-var lapChat = S.op("corner", {
-        "screen" : monLaptop,
-        "direction" : "top-left",
-        "width" : "screenSizeX/9",
-        "height" : "screenSizeY"
-});
-var lapMain = lapChat.dup({ "direction" : "top-right", "width" : "8*screenSizeX/9" });
+//
+
+// Make the window full screen on the 2nd Monitor
 var dellFull = S.op("move", {
         "screen" : monDell,
         "x" : "screenOriginX",
@@ -29,206 +26,150 @@ var dellFull = S.op("move", {
         "width" : "screenSizeX",
         "height" : "screenSizeY"
 });
-var dellTop = dellFull.dup({ "height" : "screenSizeY/2" });
-var dellIsh = dellFull.dup({
-        "width" : "screenSizeX/1.2",
-        "height" : "screenSizeY/2.3",
-        "x" : "screenOriginX + screenSizeX/14"
-    });
-var dellTopish = dellIsh.dup({
-        "y" : "screenOriginY + screenSizeY/20"
-    });
-var dellBottomish = dellIsh.dup({
-        "y" : "screenOriginY + ((screenSizeY/22) * 11)"
-    });
-var dellTopLeft = dellTopish.dup({ "width" : "screenSizeX/1.2" });
-var dellTopRight = dellTopLeft.dup({ "x" : "screenOriginX+screenSizeX/2" });
-var dellBottom = dellTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var dellBottomLeft = dellBottom.dup({ "width" : "screenSizeX/3" });
-var dellBottomMid = dellBottomLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
-var dellBottomRight = dellBottomLeft.dup({ "x" : "screenOriginX+2*screenSizeX/3" });
-var dellLeft = dellTopLeft.dup({ "height" : "screenSizeY" });
-var dellRight = dellTopRight.dup({ "height" : "screenSizeY" });
+S.bnda({ "pad5:ctrl" : dellFull, "5:ctrl" : dellFull })
+
+// Put the window in the middle of the screen and make it full height,
+// and a minimal width.
+//
+// This is primarily used for Vim
 var dellMid = dellFull.dup({ "x" : "screenSizeX/4", "width" : "screenSizeX/2" });
+S.bnda({ "pad2:ctrl" : dellMid, "2:ctrl" : dellMid })
+
+// Put the window in the middle of the screen and make it full height,
+// and medium width.
+//
+// This is primarily used for iTerm
 var dellMidWide = dellFull.dup({ "x" : "screenSizeX/6", "width" : "screenSizeX/1.5" });
+S.bnda({ "pad3:ctrl" : dellMidWide, "3:ctrl" : dellMidWide })
 
-// common layout hashes
-var lapMainHash = {
-        "operations" : [lapMain],
-        "ignore-fail" : true,
-        "repeat" : true
-};
-var adiumHash = {
-  "operations" : [lapChat, lapMain],
-  "ignore-fail" : true,
-  "title-order" : ["Contacts"],
-  "repeat-last" : true
-};
-var dellTopHash  = {
-  "operations" : [dellTop],
-  "repeat" : true
-};
-var mvimHash = {
-  "operations" : [dellTopLeft, dellTopRight],
-  "repeat" : true
-};
-var iTermHash = {
-  "operations" : [dellBottomLeft, dellBottomMid, dellBottomRight, lapMain],
-  "sort-title" : true,
-  "repeat-last" : true
-};
-var genBrowserHash = function(regex) {
-  return {
-    "operations" : [function(windowObject) {
-      var title = windowObject.title();
-      if (title !== undefined && title.match(regex)) {
-        windowObject.doOperation(hpRight);
-      } else {
-        windowObject.doOperation(lapMain);
-      }
-    }],
-    "ignore-fail" : true,
-    "repeat" : true
-  };
-}
+// Put the window at the far left and make it 50% wide
+var dellLeft = dellFull.dup({ "width" : "screenSizeX/2" });
+S.bnda({ "pad4:ctrl" : dellLeft, "4:ctrl" : dellLeft })
+var dellRight = dellLeft.dup({ "x" : "screenSizeX/2" });
+S.bnda({ "pad6:ctrl" : dellRight, "6:ctrl" : dellRight })
 
-// 2 monitor layout
-var twoMonitorLayout = S.lay("twoMonitor", {
-  "Adium" : adiumHash,
-  "MacVim" : mvimHash,
-  "iTerm" : iTermHash,
-  "Google Chrome" : lapMainHash,
-  "Xcode" : {
-    "operations" : [dellTop, lapMain],
-    "main-first" : true,
-    "repeat-last" : true
-  },
-  "Flex Builder" : dellTopHash,
-  "GitX" : lapMainHash,
-  "Ooyala Player Debug Console" : lapMainHash,
-  "Firefox" : lapMainHash,
-  "Safari" : lapMainHash,
-  "Eclipse" : dellTopHash,
-  "Spotify" : lapMainHash
-});
+// Throw windows onto another screen, making them full sized
+var throwLeftFull = S.op("throw", { "screen" : "left", "width" : "screenSizeX", "height" : "screenSizeY" })
+S.bnda({ "left:ctrl;alt;cmd" : throwLeftFull })
+var throwRightFull = S.op("throw", { "screen" : "right", "width" : "screenSizeX", "height" : "screenSizeY" })
+S.bnda({ "right:ctrl;alt;cmd" : throwRightFull })
 
-// 1 monitor layout
-var oneMonitorLayout = S.lay("oneMonitor", {
-  "Adium" : adiumHash,
-  "MacVim" : lapMainHash,
-  "iTerm" : lapMainHash,
-  "Google Chrome" : lapMainHash,
-  "Xcode" : lapMainHash,
-  "Flex Builder" : lapMainHash,
-  "GitX" : lapMainHash,
-  "Ooyala Player Debug Console" : lapMainHash,
-  "Firefox" : lapMainHash,
-  "Safari" : lapMainHash,
-  "Eclipse" : lapMainHash,
-  "Spotify" : lapMainHash
-});
+// Open the grid window sizer
+S.bnda({ "g:cmd;alt" : S.op("grid") })
 
-// Defaults
-S.def([monDell, monLaptop], twoMonitorLayout);
-S.def([monLaptop], oneMonitorLayout);
-
-// Layout Operations
-var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
-var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
-var universalLayout = function() {
-  // Should probably make sure the resolutions match but w/e
-  if (S.screenCount() === 2) {
-    twoMonitor.run();
-  } else if (S.screenCount() === 1) {
-    oneMonitor.run();
-  }
-};
-
-// Batch bind everything. Less typing.
 S.bnda({
   // Layout Bindings
-  "padEnter:ctrl" : universalLayout,
-  "space:ctrl" : universalLayout,
+  //"padEnter:ctrl" : universalLayout,
+  //"space:ctrl" : universalLayout,
 
   // Basic Location Bindings
-  "pad0:ctrl" : lapChat,
-  "[:ctrl" : lapChat,
-  "pad.:ctrl" : lapMain,
-  "]:ctrl" : lapMain,
-  "pad1:ctrl" : dellBottomLeft,
-  "1:ctrl" : dellBottomLeft,
-  "pad2:ctrl" : dellMid,
-  "2:ctrl" : dellMid,
-  "pad3:ctrl" : dellMidWide,
-  "3:ctrl" : dellMidWide,
-  "pad4:ctrl" : dellLeft,
-  "4:ctrl" : dellLeft,
-  "pad5:ctrl" : dellFull,
-  "5:ctrl" : dellFull,
-  "pad6:ctrl" : dellRight,
-  "6:ctrl" : dellRight,
-  "pad7:ctrl" : dellTopish,
-  "7:ctrl" : dellTopish,
-  "pad8:ctrl" : dellTop,
-  "8:ctrl" : dellTop,
-  "pad9:ctrl" : dellBottomish,
-  "9:ctrl" : dellBottomish,
-  "pad/:ctrl" : dellBottom,
+  //"pad1:ctrl" : dellBottomLeft,
+  //"1:ctrl" : dellBottomLeft,
+  //"pad7:ctrl" : dellTopish,
+  //"7:ctrl" : dellTopish,
+  //"pad9:ctrl" : dellBottomish,
+  //"9:ctrl" : dellBottomish,
 
-  // Resize Bindings
-  // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:ctrl" : S.op("resize", { "width" : "+10%", "height" : "+0" }),
-  "left:ctrl" : S.op("resize", { "width" : "-10%", "height" : "+0" }),
-  "up:ctrl" : S.op("resize", { "width" : "+0", "height" : "-10%" }),
-  "down:ctrl" : S.op("resize", { "width" : "+0", "height" : "+10%" }),
-  // TODO - Find new bindings for these
-  //"right:alt" : S.op("resize", { "width" : "-10%", "height" : "+0", "anchor" : "bottom-right" }),
-  //"left:alt" : S.op("resize", { "width" : "+10%", "height" : "+0", "anchor" : "bottom-right" }),
-  //"up:alt" : S.op("resize", { "width" : "+0", "height" : "+10%", "anchor" : "bottom-right" }),
-  //"down:alt" : S.op("resize", { "width" : "+0", "height" : "-10%", "anchor" : "bottom-right" }),
+  //// Resize Bindings
+  //// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+  //"right:ctrl" : S.op("resize", { "width" : "+10%", "height" : "+0" }),
+  //"left:ctrl" : S.op("resize", { "width" : "-10%", "height" : "+0" }),
+  //"up:ctrl" : S.op("resize", { "width" : "+0", "height" : "-10%" }),
+  //"down:ctrl" : S.op("resize", { "width" : "+0", "height" : "+10%" }),
+  //// TODO - Find new bindings for these
+  ////"right:alt" : S.op("resize", { "width" : "-10%", "height" : "+0", "anchor" : "bottom-right" }),
+  ////"left:alt" : S.op("resize", { "width" : "+10%", "height" : "+0", "anchor" : "bottom-right" }),
+  ////"up:alt" : S.op("resize", { "width" : "+0", "height" : "+10%", "anchor" : "bottom-right" }),
+  ////"down:alt" : S.op("resize", { "width" : "+0", "height" : "-10%", "anchor" : "bottom-right" }),
 
-  // Push Bindings
-  // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:ctrl;shift" : S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/3" }),
-  "left:ctrl;shift" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/3" }),
-  "up:ctrl;shift" : S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
-  "down:ctrl;shift" : S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
+  //// Push Bindings
+  //// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+  //"right:ctrl;shift" : S.op("push", { "direction" : "right", "style" : "bar-resize:screenSizeX/3" }),
+  //"left:ctrl;shift" : S.op("push", { "direction" : "left", "style" : "bar-resize:screenSizeX/3" }),
+  //"up:ctrl;shift" : S.op("push", { "direction" : "up", "style" : "bar-resize:screenSizeY/2" }),
+  //"down:ctrl;shift" : S.op("push", { "direction" : "down", "style" : "bar-resize:screenSizeY/2" }),
 
-  // Nudge Bindings
-  // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:ctrl;alt" : S.op("nudge", { "x" : "+10%", "y" : "+0" }),
-  "left:ctrl;alt" : S.op("nudge", { "x" : "-10%", "y" : "+0" }),
-  "up:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "-10%" }),
-  "down:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "+10%" }),
+  //// Nudge Bindings
+  //// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+  //"right:ctrl;alt" : S.op("nudge", { "x" : "+10%", "y" : "+0" }),
+  //"left:ctrl;alt" : S.op("nudge", { "x" : "-10%", "y" : "+0" }),
+  //"up:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "-10%" }),
+  //"down:ctrl;alt" : S.op("nudge", { "x" : "+0", "y" : "+10%" }),
 
-  // Throw Bindings
-  // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "pad1:ctrl;alt" : S.op("throw", { "screen" : "2", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "pad2:ctrl;alt" : S.op("throw", { "screen" : "1", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "pad3:ctrl;alt" : S.op("throw", { "screen" : "0", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "right:ctrl;alt;cmd" : S.op("throw", { "screen" : "right", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "left:ctrl;alt;cmd" : S.op("throw", { "screen" : "left", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "up:ctrl;alt;cmd" : S.op("throw", { "screen" : "up", "width" : "screenSizeX", "height" : "screenSizeY" }),
-  "down:ctrl;alt;cmd" : S.op("throw", { "screen" : "down", "width" : "screenSizeX", "height" : "screenSizeY" }),
 
-  // Focus Bindings
-  // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
-  "right:cmd;alt" : S.op("focus", { "direction" : "right" }),
-  "left:cmd;alt" : S.op("focus", { "direction" : "left" }),
-  //"up:cmd;alt" : S.op("focus", { "direction" : "up" }),
-  //"down:cmd;alt" : S.op("focus", { "direction" : "down" }),
-  //"up:cmd;alt" : S.op("focus", { "direction" : "behind" }),
-  //"down:cmd;alt" : S.op("focus", { "direction" : "behind" }),
+  //// Focus Bindings
+  //// NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
+  //"right:cmd;alt" : S.op("focus", { "direction" : "right" }),
+  //"left:cmd;alt" : S.op("focus", { "direction" : "left" }),
+  ////"up:cmd;alt" : S.op("focus", { "direction" : "up" }),
+  ////"down:cmd;alt" : S.op("focus", { "direction" : "down" }),
+  ////"up:cmd;alt" : S.op("focus", { "direction" : "behind" }),
+  ////"down:cmd;alt" : S.op("focus", { "direction" : "behind" }),
 
-  // Window Hints
-  "esc:cmd" : S.op("hint"),
+  //// Window Hints
+  //"esc:cmd" : S.op("hint"),
 
-  // Switch currently doesn't work well so I'm commenting it out until I fix it.
-  //"tab:cmd" : S.op("switch"),
+  //// Switch currently doesn't work well so I'm commenting it out until I fix it.
+  ////"tab:cmd" : S.op("switch"),
 
-  // Grid
-  "g:cmd;alt" : S.op("grid")
 });
+//var dellTop = dellFull.dup({ "height" : "screenSizeY/2" });
+//var dellIsh = dellFull.dup({
+        //"width" : "screenSizeX/1.2",
+        //"height" : "screenSizeY/2.3",
+        //"x" : "screenOriginX + screenSizeX/14"
+    //});
+//var dellTopish = dellIsh.dup({
+        //"y" : "screenOriginY + screenSizeY/20"
+    //});
+//var dellBottomish = dellIsh.dup({
+        //"y" : "screenOriginY + ((screenSizeY/22) * 11)"
+    //});
+//var dellTopLeft = dellTopish.dup({ "width" : "screenSizeX/1.2" });
+//var dellTopRight = dellTopLeft.dup({ "x" : "screenOriginX+screenSizeX/2" });
+//var dellBottomMid = dellBottomLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
+//var dellBottomRight = dellBottomLeft.dup({ "x" : "screenOriginX+2*screenSizeX/3" });
+
+//// common layout hashes
+//var dellTopHash  = {
+  //"operations" : [dellTop],
+  //"repeat" : true
+//};
+//var mvimHash = {
+  //"operations" : [dellTopLeft, dellTopRight],
+  //"repeat" : true
+//};
+//var genBrowserHash = function(regex) {
+  //return {
+    //"operations" : [function(windowObject) {
+      //var title = windowObject.title();
+      //if (title !== undefined && title.match(regex)) {
+        //windowObject.doOperation(hpRight);
+      //} else {
+        //windowObject.doOperation(lapMain);
+      //}
+    //}],
+    //"ignore-fail" : true,
+    //"repeat" : true
+  //};
+//}
+
+// Defaults
+//S.def([monDell, monLaptop], twoMonitorLayout);
+//S.def([monLaptop], oneMonitorLayout);
+
+// Layout Operations
+//var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
+//var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
+//var universalLayout = function() {
+  //// Should probably make sure the resolutions match but w/e
+  //if (S.screenCount() === 2) {
+    //twoMonitor.run();
+  //} else if (S.screenCount() === 1) {
+    //oneMonitor.run();
+  //}
+//};
+
 
 // Test Cases
 S.src(".slate.test", true);
